8942fbe99c117f6440970f8dbca81e65e0de828d
Rahman Hakim <rahmanhakim2435@protonmail.com>
2022-08-02 00:03:28 +0700

    Add functions

---
+++ basics/03_functions/src/lib.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod tests {
+
+    fn multiply(a: i32, b: i32) -> i32 {
+        todo!()
+    }
+    
+    #[test]
+    fn start() {
+        
+        let a = 10;
+        let b = 20;
+
+        let mult = multiply(a, b);
+
+        assert_eq!(200, mult);
+    }
+}
